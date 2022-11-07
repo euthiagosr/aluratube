f74a3d346135492ff31fec613574f077ae7bb220
Thiago <thiago.a.rodrigues@accenture.com>
2022-11-07 20:53:48 +0000

    first commit

---
+++ pages/index.js
@@ -0,0 +1,81 @@
+import config from "../config.json";
+import styled from "styled-components";
+import { CSSReset } from "../src/components/CSSReset";
+import { StyledTimeline } from "../src/components/Timeline";
+import Menu from "../src/components/Menu"; 
+
+function HomePage() {
+  return (
+    <>
+      <CSSReset />
+      <div style={{
+        display: "flex",
+        flexDirection: "column",
+        flex: 1
+      }}>
+        <Menu />
+        <Header />
+        <Timeline playlists={config.playlists} />
+      </div>
+    </>
+  );
+}
+
+export default HomePage;
+
+const StyledHeader = styled.div`
+  img {
+    width: 80px;
+    height: 80px;
+    border-radius: 50%;
+  }
+  .user-info {
+    display: flex;
+    align-items: center;
+    width: 100%;
+    padding: 16px 32px;
+    gap: 16px;
+    margin-top: 50px;
+  }
+`;
+function Header() {
+  return (
+    <StyledHeader>
+      <section className="user-info">
+        {/*<img src="banner" />*/}
+        <img src={`https://github.com/${config.github}.png`} />
+        <div>
+          <h2>{config.name}</h2>
+          <p>{config.job}</p>
+        </div>
+      </section>
+    </StyledHeader>
+  );
+}
+
+function Timeline(props) {
+  const playlists = Object.keys(props.playlists);
+
+  return (
+    <StyledTimeline>
+      {playlists.map((playlistName) => {
+        const videosLst = props.playlists[playlistName];
+        return (
+          <section>
+            <h2 style={{ textTransform: "capitalize" }}>{playlistName}</h2>
+            <div>
+              {videosLst.map((video) => {
+                return (
+                  <a href={video.url}>
+                    <img src={video.thumb} />
+                    <span>{video.title}</span>
+                  </a>
+                );
+              })}
+            </div>
+          </section>
+        );
+      })}
+    </StyledTimeline>
+  );
+}
